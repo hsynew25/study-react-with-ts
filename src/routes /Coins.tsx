7507339ee1bc674ffbd6895e773e98c3bcf1dd1d
hsy <hsynew25@gmail.com>
2022-04-08 17:18:32 +0900

    [init]

---
+++ src/routes /Coins.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { fetchCoins } from "../api";
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 20px auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  margin-bottom: 10px;
+  border-radius: 10px;
+  a {
+    transition: color 0.2s ease-in-out;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+  }
+
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Loader = styled.div`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface ICoin {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
+
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {isLoading ? (
+        <Loader>LOADING...</Loader>
+      ) : (
+        <CoinsList>
+          {data?.slice(0, 20).map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}/price`,
+                  state: {
+                    coin,
+                  },
+                }}
+              >
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
+}
+
+export default Coins;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#487eb0",
+  textColor: "#f5f6fa",
+  accentColor: "#fbc531",
+};
