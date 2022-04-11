db5248fb66f5451659e4835072ee2af18a55e7ba
hsy <hsynew25@gmail.com>
2022-04-11 22:01:47 +0900

    [추가] apexcharts 그래프 추가

--- src/routes /Coin.tsx
+++ src/routes /Coin.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
+import { Helmet } from "react-helmet";
 import {
   Link,
   Route,
@@ -121,23 +121,25 @@ interface PriceData {
   first_data_at: string;
   last_updated: string;
   quotes: {
-    ath_date: string;
-    ath_price: number;
-    market_cap: number;
-    market_cap_change_24h: number;
-    percent_change_1h: number;
-    percent_change_1y: number;
-    percent_change_6h: number;
-    percent_change_7d: number;
-    percent_change_12h: number;
-    percent_change_15m: number;
-    percent_change_24h: number;
-    percent_change_30d: number;
-    percent_change_30m: number;
-    percent_from_price_ath: number;
-    price: number;
-    volume_24h: number;
-    volume_24h_change_24h: number;
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
   };
 }
 
@@ -152,12 +154,21 @@ function Coin() {
   );
   const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
     ["tickers", coinId],
-    () => fetchCoinTickers(coinId)
+    () => fetchCoinTickers(coinId),
+    {
+      refetchInterval: 1000 * 60,
+    }
   );
   const loading = infoLoading || tickersLoading;
 
   return (
     <Container>
+      <Helmet>
+        <title>
+          {" "}
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </title>
+      </Helmet>
       <Header>
         <Title>
           {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
@@ -177,8 +188,8 @@ function Coin() {
               <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
-              <span>Open Source:</span>
-              <span>{infoData?.open_source ? "Yes" : "No"}</span>
+              <span>Price:</span>
+              <span>{tickersData?.quotes.USD.price}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
@@ -207,7 +218,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
